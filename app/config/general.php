f2d8d5320fb7bbb6d35831ccd152d33558a4dff3
emmy seco <42155199+lfirebw@users.noreply.github.com>
2020-08-23 17:32:08 -0500

    Update general.php

--- app/config/general.php
+++ app/config/general.php
@@ -1,8 +1,8 @@
 <?php
 return array(
     'root' => '/fireframework',
-    'layout' => 'default',
-    'isLogin' => false,
-    'onlyRouting' => false
+    'theme' => 'default',
+    'modules'=> [],
+    'cache' => false
 );
 ?>
